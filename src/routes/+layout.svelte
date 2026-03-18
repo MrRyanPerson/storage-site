8147dac76b14d4a699151842ca8b844035dc09d1
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-03-18 00:00:42 +0000

    Add fallback 404 page and seo improvements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,5 +8,9 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>Storage Share</title>
+	<meta name="description"
+    content="A simple app for uploading and sharing files in the cloud" />
+	<meta name="keywords" content="file sharing, cloud storage, upload, download" />
+	<meta name="author" content="mrryanperson" />
 </svelte:head>
 {@render children()}
