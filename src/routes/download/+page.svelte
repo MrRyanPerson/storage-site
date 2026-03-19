2802f33e976bc8212d396cf2697aeafe0240f9c3
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-03-19 04:04:18 +0000

    Autocapitalize download key and small stylistic improvements

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -7,7 +7,6 @@
 
 
     async function downloadFiles(key) {
-        if (typeof window === 'undefined') return;
 
         let fileList = await fetch(`https://storage-worker.ryanh41137.workers.dev/files/${key}`);
         fileList = await fileList.json();
@@ -25,6 +24,7 @@
             // Prevent human error by replacing common mistakes in the key
             key = key.replace(' ', '');
             key = key.replace('I', '1');
+            key = key.toUpperCase();
             downloadFiles(key);
         } else {
             alert("Download cancelled.");
@@ -42,8 +42,8 @@
     <form class="h-60 w-96 rounded-md border p-4 mb-2" onsubmit={submitKey}>
         <h2 class="text-2xl font-semibold text-heading mb-2">Download Files</h2>
         <p class="text-body mb-2">Enter the key to download your files.</p>
-        <div class="relative z-0 w-full mb-5 group">
-            <input type="text" name="key" id="key" class="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
+        <div class="relative z-0 w-full mb-6 group">
+            <input type="text" name="key" id="key" class="block py-2.5 px-0 w-full text-sm text-heading uppercase font-semibold bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
             <label for="key" class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Key</label>
         </div>
         <button type="submit" class="text-body cursor-pointer bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-full text-sm px-4 py-2.5 focus:outline-none">Download</button>
